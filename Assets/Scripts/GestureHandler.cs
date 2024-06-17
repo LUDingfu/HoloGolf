e0e9088ba3947b7d02909d97012392a13d2f2406
LUDingfu <ludingfu990088@gmail.com>
2024-06-17 11:05:47 -0700

    the script of GolfBallController is the final version control by keyboard

---
+++ Assets/Scripts/GestureHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine.Serialization;
+
+public class GestureHandler : MonoBehaviour, IMixedRealityPointerHandler
+{
+    [SerializeField] private GolfBallController ballController;
+    
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+       GolfBallController.
+    }
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+       
+    }
+
+}
